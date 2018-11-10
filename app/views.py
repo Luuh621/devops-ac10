84039697b7436863ccd6816d1a422269ca9fd9ce
Luuh621 <42192140+Luuh621@users.noreply.github.com>
2018-11-10 14:33:25 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -84,7 +84,7 @@ def cadastro_candidatos(request):
         context_instance = RequestContext(request, 
         {
             'title':'Cadastro de candidatos',
-            'candidato': candidato.objects.all( ),
+            'candidato': Candidato.objects.all( ),
             'year':datetime.now().year, 
         })
     )
