daa3eea1220ac017688c489d901078e55aba2f82
Luuh621 <42192140+Luuh621@users.noreply.github.com>
2018-11-09 22:17:09 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -12,5 +12,6 @@ class Curso(models.Model):
 
 class Vestibular(models.Model):
     nome = models.CharField(max_length=200)
+    
 class Candidatos(models.Model):
-    nome=models.CharField(max_length=200)
+    nome = models.CharField(max_length=200)
